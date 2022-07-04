405cae93f46d10e81f5fcecf2c37816d05c686fb
Paul Hung <paulhonghaha@gmail.com>
2022-07-04 15:38:00 -0700

    Update solution100

--- solution100.cc
+++ solution100.cc
@@ -4,6 +4,7 @@
 
 namespace Leetcode {
 bool Solution100::isSameTree(TreeNode* p, TreeNode* q) {
+    // TC:O(N), SC:O(1)
     if (p != nullptr && q != nullptr) {
         return (p->val == q->val)&&isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
     }
