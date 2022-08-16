6f12e4e2d3514c511033b1de68237a03053bf7f2
Paul Hung <paulhonghaha@gmail.com>
2022-08-15 21:20:31 -0700

    Add solution117

---
+++ solution117.cc
@@ -0,0 +1,31 @@
+#include "solution117.h"
+
+#include <queue>
+
+#include "Node.h"
+
+Node* Solution117::connect(Node* root) {
+    // TC:O(N), SC:O(N)
+    std::queue<Node*> q;
+    if (root != nullptr) {
+        q.push(root);
+    }
+    
+    while (!q.empty()) {
+        int cur_size = q.size();
+        Node* prev = nullptr;
+        for (int i = 0; i < cur_size; i++) {
+            Node* cur = q.front();
+            cur->next = prev;
+            prev = cur;
+            if (cur->right != nullptr) {
+                q.push(cur->right);
+            }
+            if (cur->left != nullptr) {
+                q.push(cur->left);
+            }
+            q.pop();
+        }
+    }
+    return root;
+}
---
+++ solution117.h
@@ -0,0 +1,13 @@
+// Leetcode 117: Populating Next Right Pointers in Each Node II
+#ifndef LEETCODE_SOLUTION117
+#define LEETCODE_SOLUTION117
+#include "Node.h"
+
+namespace Leetcode {
+class Solution117 {
+public:
+    Node* connect(Node* root);
+};
+}   // namespace Leetcode
+
+#endif
