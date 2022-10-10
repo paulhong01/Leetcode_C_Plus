a432324c396b274531d675bd15e96d5e9b257c21
Paul Hung <paulhonghaha@gmail.com>
2022-10-09 19:45:24 -0700

    Add solution128

---
+++ solution128.h
@@ -0,0 +1,14 @@
+// Leetcode 128: Longest Consecutive Sequence
+#ifndef LEETCODE_SOLUTION128
+#define LEETCODE_SOLUTION128
+
+#include <vector>
+
+namespace Leetcode {
+class Solution128 {
+public:
+    int longestConsecutive(std::vector<int>& nums);
+};
+}   // namespace
+
+#endif
