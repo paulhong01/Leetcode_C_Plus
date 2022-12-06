9e151856b222ae52b57048edbf4b094bd3211e25
Paul Hung <paulhonghaha@gmail.com>
2022-12-05 20:37:18 -0800

    Add solution142

---
+++ solution142.h
@@ -0,0 +1,12 @@
+// Leetcode 142: Linked List Cycle II
+#ifndef LEETCODE_SOLUTION142
+#define LEETCODE_SOLUTION142
+
+namespace Leetcode {
+class Solution142 {
+public:
+    ListNode* detectCycle(ListNode* head);
+};
+}   // namespace Leetcode
+
+#endif
