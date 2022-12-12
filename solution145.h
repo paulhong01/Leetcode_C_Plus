2a75ce34a8c7116140d760e962100369e263b644
Paul Hung <paulhonghaha@gmail.com>
2022-12-11 19:37:11 -0800

    Add solution145

---
+++ solution145.h
@@ -0,0 +1,12 @@
+// Leetcode 145: Binary Tree Postorder Traversal
+#ifndef LEETCODE_SOLUTION145
+#define LEETCODE_SOLUTION145
+
+namespace Leetcode {
+class Solution146 {
+public:
+    vector<int> postorderTraversal(TreeNode* root);
+};
+}
+
+#endif
