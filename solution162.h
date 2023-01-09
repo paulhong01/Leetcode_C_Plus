ecd4ac791ba9e2c143cf68c1872a4d0eabf72779
Paul Hung <paulhonghaha@gmail.com>
2023-01-08 19:12:23 -0800

    Add solution162

---
+++ solution162.h
@@ -0,0 +1,14 @@
+// Leetcode 162: Find Peak Element
+#ifndef LEETCODE_SOLUTION162
+#define LEETCODE_SOLUTION162
+
+namespace Leetcode {
+
+class Solution162 {
+public:
+    int findPeakElement(std::vector<int>& nums);
+};
+
+}   // namespace Leetcode
+
+#endif
