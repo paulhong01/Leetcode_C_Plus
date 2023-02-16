9b146802f5ac99b8c0f9f78d2276d3e9df867c81
Paul Hung <paulhonghaha@gmail.com>
2023-02-15 22:27:21 -0800

    Add solution188

---
+++ solution188.h
@@ -0,0 +1,16 @@
+// Leetcode 188: Best Time to Buy and Sell Stock IV
+#ifndef LEETCODE_SOLUTION188
+#define LEETCODE_SOLUTION188
+
+#include <vector>
+
+namespace Leetcode {
+
+class Solution188 {
+public:
+    int maxProfit(int k, std::vector<int>& prices);
+};
+
+}   // namespace Leetcode
+
+#endif
