b5ff4773d6105091e5d611612bcbf319215943a0
Paul Hung <paulhonghaha@gmail.com>
2023-07-16 17:36:22 -0700

    Add solution2748

---
+++ solution2748.h
@@ -0,0 +1,12 @@
+// Leetcode 2748: Number of Beautiful Pairs
+
+#include <vector>
+
+namespace Leetcode {
+
+class Solution2748 {
+public:
+    int countBeautifulPairs(std::vector<int>& nums);
+};
+
+}   // namespace Leetcode
