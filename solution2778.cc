bd6840091bd99e71d895279599970c6c5ef6fcdd
Paul Hung <paulhonghaha@gmail.com>
2023-07-30 15:17:58 -0700

    Update solution2778

--- solution2778.cc
+++ solution2778.cc
@@ -3,6 +3,7 @@
 namespace Leetcode {
 
 int sumOfSquares(std::vector<int>& nums) {
+    // TC:O(N), SC:O(1)
     int ret = 0, N = nums.size();
     for (int i = 1; i <= N; i++) {
         if (N%i != 0) {
