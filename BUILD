1009da6ef6d2c054fc27901933d87e560f2823b4
Paul Hung <paulhonghaha@gmail.com>
2024-01-14 14:51:12 -0800

    Add solution2901

--- BUILD
+++ BUILD
@@ -877,6 +877,12 @@ cc_library(
     srcs = ["solution2895.cc"],
 )
 
+cc_library(
+    name = "solution2901",
+    hdrs = ["solution2901.h"],
+    srcs = ["solution2901.cc"],
+)
+
 cc_test(
   name = "solution87_test",
   size = "small",
---
+++ solution2901.h
@@ -0,0 +1,17 @@
+// Leetcode 2901: Longest Unequal Adjacent Groups Subsequence II
+#ifndef LEETCODE_SOLUTION2901
+#define LEETCODE_SOLUTION2901
+
+#include <vector>
+#include <string>
+
+namespace Leetcode {
+
+class Solution2901 {
+public:
+    std::vector<std::string> getWordsInLongestSubsequence(int n, std::vector<std::string>& words, std::vector<int>& groups);
+};  
+
+}   // namespace Leetcode
+
+#endif
